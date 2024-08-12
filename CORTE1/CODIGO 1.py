32132a3d2077a7ea5ac42a840b112ce623e4727f
Samuel Garcia <76258324+GarcoPro23@users.noreply.github.com>
2024-08-12 07:48:41 -0500

    Add files via upload

---
+++ CORTE1/CODIGO 1.py
@@ -0,0 +1,10 @@
+print("Mi primera vez con Python")
+a = "Cadena de caracteres"
+print ("Tipo dato a: " , type (a))
+b = 8
+print ("Tipo dato b: " , type (b))
+c = 3.1415
+print ("Tipo dato c: " , type (c))
+print ("Dato a: " ,a)
+print ("Dato b: " ,b)
+print ("Dato c: " ,c)
